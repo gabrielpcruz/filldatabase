95ac40f635780af49997a2afabcd1d68501bcccc
Gabriel Cruz <gabrielcruzsi.mail@gmail.com>
2019-07-17 11:52:24 -0300

    PHPDoc

--- app/models/Connection.php
+++ app/models/Connection.php
@@ -6,8 +6,16 @@ use Illuminate\Database\Capsule\Manager;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Container\Container;
 
+/**
+ * Class Connection
+ * @package app\models
+ */
 class Connection
 {
+    /**
+     * Connection constructor.
+     * @param $configurations
+     */
     public function __construct($configurations)
     {
         $capsule = new Manager();
